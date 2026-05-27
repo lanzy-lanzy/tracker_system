3089bab9b2b741c2d4b3f67c03700aa523b472be
lanzy-lanzy <gerlan.masterpay@gmail.com>
2026-05-28 00:53:21 +0800

    Disable DRF pagination to fix mobile app list endpoints

--- tracker_system/settings.py
+++ tracker_system/settings.py
@@ -240,8 +240,8 @@ REST_FRAMEWORK = {
         "anon": "100/hour",
         "user": "1000/hour",
     },
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 50,
+    "DEFAULT_PAGINATION_CLASS": None,
+
 }
 
 from datetime import timedelta
