6e8325eef3c9f3a9d9d25249833b4a212b264372
lanzy-lanzy <gerlan.masterpay@gmail.com>
2026-05-28 00:34:26 +0800

    Move admin seed to AppConfig.ready() for runtime execution

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,20 @@
+import os
 from django.apps import AppConfig
-
+from django.contrib.auth import get_user_model
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        User = get_user_model()
+        password = os.environ.get("SEED_ADMIN_PASSWORD")
+        if password and not User.objects.filter(is_superuser=True).exists():
+            try:
+                User.objects.create_superuser(
+                    username="admin",
+                    email="admin@example.com",
+                    password=password,
+                )
+            except Exception:
+                pass
--- build_files.sh
+++ build_files.sh
@@ -4,4 +4,3 @@ set -e
 python -m pip install --break-system-packages -r requirements.txt
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-python manage.py ensure_admin
