7c80c4e6ee04cef30466dea989a3cc50e90e39a0
lanzy-lanzy <gerlan.masterpay@gmail.com>
2026-05-28 17:53:09 +0800

    Fix build script: add pip install for Vercel static build

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+python -m pip install --break-system-packages -r requirements.txt
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 python manage.py seedadmin
