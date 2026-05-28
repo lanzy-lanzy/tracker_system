0be2b599c79c40e513582ecce56b7475e751c514
lanzy-lanzy <gerlan.masterpay@gmail.com>
2026-05-28 17:56:52 +0800

    Move migrate/seedadmin to WSGI startup; build no longer needs DB

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,4 @@
 set -e
 
 python -m pip install --break-system-packages -r requirements.txt
-python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-python manage.py seedadmin
--- tracker_system/wsgi.py
+++ tracker_system/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tracker_system.settings")
 
 application = get_wsgi_application()
+
+if os.environ.get("VERCEL") and not os.environ.get("WSGI_INIT_DONE"):
+    os.environ["WSGI_INIT_DONE"] = "1"
+    from django.core.management import call_command
+    try:
+        call_command("migrate", "--noinput")
+        call_command("seedadmin")
+    except Exception as e:
+        print(f"WSGI startup: {e}", file=sys.stderr)
